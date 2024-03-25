e1db9605b64cf46815453d041a9e307067b08ada
Abdalla <fabricioabdcosta@gmail.com>
2024-03-24 21:06:25 -0400

    correção de bugs

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { NavigationContainer } from '@react-navigation/native';
+  import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // importação das telas
